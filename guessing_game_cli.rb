eae8e5905ecd29eb34745c485dbaf41dff80fb4f
lior Shine <shinelior@gmail.com>
2020-02-03 16:40:51 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,22 +1,25 @@
 # Code your solution here!
-#require_relative 'spec_helper'
-#require_relative "../guessing_game_cli"
+
 require "pry"
 def run_guessing_game
-  
-  puts "please guess a number between 1-6"
+  random_number = rand(6) + 1
+  #puts "please guess a number between 1-6"
+
   player_guess = gets.chomp
-  random_number = rand(1..6)
+  
    if   player_guess == "exit"
        puts  "Goodbye!"
- elsif player_guess.to_i == random_number
-    puts "You guessed the correct number!" 
- 
+ elsif player_guess.to_i != random_number
+  puts "/Sorry! The computer guessed #{random_number}/"
+  # print /Sorry! The computer guessed 6./
+   
   else 
-   puts  "Sorry! The computer guessed #{random_number}"
-
+   
+    print /You guessed the correct number!/
+    
+    
   end
-  #binding.pry
+#binding.pry
 
  
 
